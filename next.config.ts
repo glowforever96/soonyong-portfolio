609d3f6073cc0ca346b7b2f89abe1650a3ccacef
boyfromthewell <tnsdyd10@naver.com>
2024-11-13 14:54:02 +0900

    feat: styled-component uninstall, vanilla extract css 도입

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from 'next';
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
+const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
@@ -17,4 +19,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
