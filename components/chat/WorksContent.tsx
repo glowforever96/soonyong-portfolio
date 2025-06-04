bf05384d07b0a76108753658842df8141b07731e
boyfromthewell <tnsdyd10@naver.com>
2025-06-04 13:36:59 +0900

    fix: 이력 설명 수정

--- components/chat/WorksContent.tsx
+++ components/chat/WorksContent.tsx
@@ -15,58 +15,60 @@ export default function WorksContent({ type }: { type: 'BUBBLE' | 'FILL' }) {
   return (
     <>
       <div className={styles.workBoxWrapper[type]}>
-        {WORK_DATA.map(({ title, link, date, tasks, skillsets, desc, img }) => (
-          <div className={styles.worksBox} key={title}>
-            <div className={styles.workTitleBox}>
-              <p className={styles.workTitle}>{title}</p>
-              {link && (
-                <Link href={link} target="_blank" className={styles.aTag}>
-                  {link}
-                </Link>
-              )}
-              <span className={styles.date}>{date}</span>
-              {type === 'FILL' && img && (
-                <ScrollXslider>
-                  <div className={styles.imgsContainer}>
-                    {img.map((src, idx) => (
-                      <Image
-                        style={{ cursor: 'pointer' }}
-                        title="크게 보기"
-                        key={idx}
-                        src={src}
-                        alt="project img"
-                        priority
-                        width={500}
-                        height={400}
-                        className={styles.projectsImgs}
-                        onClick={() => {
-                          setImgSrc(src);
-                          openPopup();
-                        }}
-                      />
-                    ))}
+        {WORK_DATA.map(
+          ({ title, subTitle, link, date, tasks, skillsets, img }) => (
+            <div className={styles.worksBox} key={title}>
+              <div className={styles.workTitleBox}>
+                <p className={styles.workTitle}>{title}</p>
+                {link && (
+                  <Link href={link} target="_blank" className={styles.aTag}>
+                    {link}
+                  </Link>
+                )}
+                <span className={styles.date}>{date}</span>
+                <p>{subTitle}</p>
+                {type === 'FILL' && img && (
+                  <ScrollXslider>
+                    <div className={styles.imgsContainer}>
+                      {img.map((src, idx) => (
+                        <Image
+                          style={{ cursor: 'pointer' }}
+                          title="크게 보기"
+                          key={idx}
+                          src={src}
+                          alt="project img"
+                          priority
+                          width={500}
+                          height={400}
+                          className={styles.projectsImgs}
+                          onClick={() => {
+                            setImgSrc(src);
+                            openPopup();
+                          }}
+                        />
+                      ))}
+                    </div>
+                  </ScrollXslider>
+                )}
+              </div>
+              <p className={styles.worksTaskTitle}>Tasks</p>
+              <ul className={styles.ulContainer}>
+                {tasks.map((text) => (
+                  <li key={text} className={styles.liTagBig}>
+                    {text}
+                  </li>
+                ))}
+              </ul>
+              <div className={styles.skillsetWrapper}>
+                {skillsets.map((text) => (
+                  <div key={text} className={styles.skillChip}>
+                    {text}
                   </div>
-                </ScrollXslider>
-              )}
+                ))}
+              </div>
             </div>
-            <p className={styles.worksTaskTitle}>Tasks</p>
-            <ul className={styles.ulContainer}>
-              {tasks.map((text) => (
-                <li key={text} className={styles.liTagBig}>
-                  {text}
-                </li>
-              ))}
-            </ul>
-            <div className={styles.skillsetWrapper}>
-              {skillsets.map((text) => (
-                <div key={text} className={styles.skillChip}>
-                  {text}
-                </div>
-              ))}
-            </div>
-            <p className={styles.workDesc}>💡 {desc}</p>
-          </div>
-        ))}
+          ),
+        )}
       </div>
       {isPopupOpen && (
         <Modal type="IMG">
